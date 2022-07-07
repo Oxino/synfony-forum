91bde6c29e7c868dbc37d410a67660fa25c087b6
nomuser <user@gmail.com>
2022-07-07 16:01:39 +0200

    edit category

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -7,9 +7,12 @@ use App\Repository\CategoryRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+use Doctrine\ORM\EntityManagerInterface;
+use App\Form\CategoryFormType;
 
 #[Route('/category', name: 'category-')]
-class CategoryController extends AbstractController
+class CategoryController extends AbstractController 
 {
     #[Route('/', name: 'main')]
     public function index(CategoryRepository $categoryRepository): Response
@@ -32,6 +35,23 @@ class CategoryController extends AbstractController
         return $this->render('category/category.html.twig', [
             'category' => $category,
             'tickets' => $category->getTickets(),
+        ]);
+    }
+
+    #[Route('/edit/{slug}', name: 'editBySlug')]
+    public function edit(Category $category, Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $form = $this->createForm(CategoryFormType::class, $category);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($category);
+            $entityManager->flush();
+        }
+
+        return $this->render('category/edit.html.twig', [
+            'category' => $category,
+            'categoryForm' => $form->createView(),
         ]);
     }
 }
